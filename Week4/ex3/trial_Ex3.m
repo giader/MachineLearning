d1b2fc3c34ed212ee0e9c5965c9a664db2bc1285
Gianni De Rossi <giaderblog@gmail.com>
2020-01-02 19:27:24 +0100

    Add files via upload

---
+++ Week4/ex3/trial_Ex3.m
@@ -0,0 +1,22 @@
+cd 'C:\Users\Gianni\Dropbox\MOOC-Coursera\Coursera-MachineLearning\Week4\machine-learning-ex3'
+
+input_layer_size  = 400;  % 20x20 Input Images of Digits
+num_labels = 10;          % 10 labels, from 1 to 10
+                          % (note that we have mapped "0" to label 10)
+
+% Load Training Data
+load('ex3data1.mat'); % training data stored in arrays X, y
+m = size(X, 1);
+
+% Randomly select 100 data points to display
+rand_indices = randperm(m);
+sel = X(rand_indices(1:10), :);
+
+theta_t = [-2; -1; 1; 2];
+X_t = [ones(5,1) reshape(1:15,5,3)/10];
+y_t = ([1;0;1;0;1] >= 0.5);
+lambda_t = 3;
+
+% regularize theta
+theta_reg = [0;theta_t(2:end, :);];
+
